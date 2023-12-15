cd6118718c1bf5725eb8e42376f1cd9ce5ee2511
HoziKwitty <zoesparg@icloud.com>
2023-12-15 15:59:02 +0200

    Added custom tag

--- src/main/java/net/hozikwitty/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/hozikwitty/tutorialmod/item/custom/MetalDetectorItem.java
@@ -1,5 +1,6 @@
 package net.hozikwitty.tutorialmod.item.custom;
 
+import net.hozikwitty.tutorialmod.util.ModTags;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -11,7 +12,6 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
@@ -66,6 +66,6 @@ public class MetalDetectorItem extends Item {
     }
 
     private boolean isValuableBlock(BlockState state) {
-        return state.is(Blocks.IRON_ORE);
+        return state.is(ModTags.Blocks.METAL_DETECTOR_VALUABLES);
     }
 }
